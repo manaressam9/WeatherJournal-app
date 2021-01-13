c599fe230c4b69fdca85e3745d791ea23cc794e2
manaressam9 <47770516+manaressam9@users.noreply.github.com>
2021-01-13 18:28:06 +0200

    Add files via upload

---
+++ weather-journal-app/server.js
@@ -0,0 +1,34 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express, body-parser and cors to run server and routes
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port,() => console.log(`running on localhost: ${port}`));
+/* Routes */
+//Get Route named "getdata" to return data stored in projectData object
+app.get('/getdata', (request,response) => response.send(projectData).status(200).end());
+//Post Route named "savedata" to save data received from the API
+app.post('/savedata', (request,response) => {
+ projectData= {
+     temp: request.body.temp,
+     cont: request.body.cont,
+     date: request.body.date
+ };
+ response.send(projectData).status(200).end();
+});
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,76 @@
+/* Global Variables */
+//variables for elements
+const zipElem = document.getElementById('zip');
+const btnElem = document.getElementById('generate');
+const feelingsElem = document.getElementById('feelings');
+const dateElem = document.getElementById('date');
+const tempElem = document.getElementById('temp');
+const contElem = document.getElementById('content');
+//Api and localserver url
+const apiKey = "&appid=7c22738f56e50fe06a8d3b4f84b2029f&units=metric";
+const baseUrl = "http://localhost:8000/";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//add Eventlistener on click on myBtn to send zipcode to api and save/post the response of api to our local server 
+btnElem.addEventListener('click',() => {
+ let maData = {
+     zip: zipElem.value,
+     cont: feelingsElem.value,
+     date: newDate
+ };
+ //get temp info 
+ apiResponse(maData.zip).then(zipInfo => {
+     //checks if zip is correct 
+  if(zipInfo.cod != 200){                     //  if not ,alert with place not found  
+      alert('place not found')
+  }else{
+    maData.temp = zipInfo.list[0].main.temp;    // if exist,then add temp to maData and save data to the server
+    saveToMyServer(maData);
+  }
+     
+}).catch(errorCatcher);
+ 
+});
+
+/*functions*/
+function errorCatcher(error){
+    console.error('ERROR',error)
+}
+//-----------
+async function apiResponse(zip){
+    return await (await fetch(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip}${apiKey}`)).json()
+}
+//------------
+async function saveToMyServer(maData){
+    let response = await fetch(`${baseUrl}savedata`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(maData),
+    });
+    try{
+        response.json().then( maData => {
+            if(!response.ok){
+                alert('data not saved')
+            }else{
+                showmaData();
+            }
+        }).catch(errorCatcher);
+    }catch(error){
+      errorCatcher(error);
+    }
+}
+//at last update out applocation UI
+async function showmaData(){
+    let response = await fetch(`${baseUrl}getdata`);
+    try{
+       response.json().then(maData => {
+        tempElem.innerText = `temperature in celisus: ${maData.temp}`;
+        dateElem.innerText = `Date: ${maData.date}`;
+        contElem.innerText = `feelings: ${maData.cont}`
+       }).catch(errorCatcher);
+    }catch(error){
+      errorCatcher(error);
+    }
+}
